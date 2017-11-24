03cb18ef2db500b22f9fd39e37c4aa2c8d188d9f
luck17291 <luck17291@yahoo.com>
2017-11-24 16:12:16 +0700

    e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,124 @@
 import { AppPage } from './app.po';
+import { Browser } from 'selenium-webdriver';
+import { browser, protractor, element, by } from 'protractor';
 
 describe('todolist App', () => {
   let page: AppPage;
+  var EC = protractor.ExpectedConditions;
+  let defaultWaitTime: number = 5000;
 
   beforeEach(() => {
     page = new AppPage();
+
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+
+  this.waitUntilReady = function (elm, timeOut = defaultWaitTime) {
+    browser.wait(function () {
+      return elm.isPresent();
+    }, timeOut);
+    browser.wait(function () {
+      return elm.isDisplayed();
+    }, timeOut);
+  };
+
+  // it('should show progess bar', () => {
+
+  //   page.navigateTo().then(() => {
+  //     browser.wait(page.getProgressBar().isPresent(),5000).then(() => {
+  //       expect(page.getProgressBar().isPresent()).toBeTruthy();
+  //     })
+
+  //   })
+
+  //   // expect(page.getProgressBar().isPresent()).toBeTruthy();
+
+  //   // this.waitUntilReady(page.getProgressBar())
+
+  //   // browser.wait(page.getProgressBar().isPresent(),10000).then(()=>{
+  //   //   expect(page.getProgressBar().isPresent()).toBe(true);
+  //   // })
+
+  //   // browser.wait(EC.presenceOf(page.getProgressBar()), 10000).then(() => {
+  //   //   expect(page.getProgressBar().isPresent()).toBe(true);
+  //   // })
+
+  // });
+
+  describe('add new task', () => {
+    let numberOfTasksBeforeAdd = 0;
+    let numberOfTasksAfterAdd = 0;
+
+    it('should show open new task dialog when click add new task', () => {
+      page.navigateTo();
+      page.getAllTaskElement().count().then(number => this.numberOfTasksBeforeAdd = number);
+
+      page.getAddTaskButton().click();
+      expect(page.getDialogTitle().isDisplayed()).toBe(true);
+      expect(page.getDialogTitle().getText()).toEqual("Add new Task");
+    });
+
+    it('should show progess bar when click save on dialog and number of task should increase', () => {
+      page.getDialogTaskName().sendKeys("new task");
+      page.getDialogSaveBtn().click();
+      // this.waitUntilReady(page.getProgressBar());
+      // expect(page.getProgressBar().isPresent()).toBe(true);
+      browser.sleep(3000);
+      page.getAllTaskElement().count()
+        .then(number => this.numberOfTasksAfterAdd = number)
+        .then(() => {
+          expect(this.numberOfTasksAfterAdd).toEqual(this.numberOfTasksBeforeAdd + 1);
+        })
+    });
+
+    it('should new task on list', () => {
+      let lastestIndex = this.numberOfTasksBeforeAdd + 1;
+      let newTask = page.getAllTaskElement().last();
+      expect(newTask.getText()).toEqual("new task");
+    });
   });
+
+  describe('update task', () => {
+
+    it('should show open update task dialog when click on task', () => {
+      page.navigateTo();
+      page.getAllTaskElement().first().click();
+      expect(page.getDialogTitle().isDisplayed()).toBe(true);
+      expect(page.getDialogTitle().getText()).toEqual("Update Task");
+    });
+
+    it('should show progess bar when click save on dialog and task name have changed', () => {
+      page.getDialogTaskName().clear();
+      page.getDialogTaskName().sendKeys("task 123");
+      page.getDialogSaveBtn().click();
+      // this.waitUntilReady(page.getProgressBar());
+      // expect(page.getProgressBar().isPresent()).toBe(true);
+      browser.sleep(3000);
+      expect(page.getAllTaskElement().first().getText()).toEqual("task 123");
+    });
+  });
+
+  describe('delete task', () => {
+    let numberOfTasksBeforeDelete: number = 0;
+    let numberOfTasksAfterDelete: number = 0;
+
+    it('should show progess bar when click delete task and tasks number decrease', () => {
+      page.navigateTo();
+      page.getAllTaskElement().count()
+        .then(number => this.numberOfTasksBeforeDelete = number)
+        .then(() => {
+          page.getDeleteButton().click();
+          browser.sleep(3000);
+
+          page.getAllTaskElement().count()
+            .then(number => this.numberOfTasksAfterDelete = number)
+            .then(() => {
+              expect(this.numberOfTasksAfterDelete).toEqual(this.numberOfTasksBeforeDelete - 1);
+            });
+        })
+
+    });
+  });
+
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,42 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('.e2e-app-title')).getText();
+  }
+
+  getReloadButton() {
+    return element(by.css('.e2e-reload-btn'));
+  }
+
+  getAddTaskButton() {
+    return element(by.css('.e2e-add-task-btn'));
+  }
+
+  getDeleteButton() {
+    return element(by.css('.e2e-delete-task-btn'));
+  }
+
+  getProgressBar() {
+    return element(by.css('.e2e-progess-bar'));
+  }
+
+  getTaskElement() {
+    return element(by.css('.e2e-task'));
+  }
+
+  getAllTaskElement() {
+    return element.all(by.css('.e2e-task'));
+  }
+
+  getDialogTitle() {
+    return element(by.css('.e2e-dialog-title'));
+  }
+
+  getDialogTaskName() {
+    return element(by.css('.e2e-dialog-task-name'));
+  }
+
+  getDialogSaveBtn() {
+    return element(by.css('.e2e-dialog-save-btn'));
   }
 }
